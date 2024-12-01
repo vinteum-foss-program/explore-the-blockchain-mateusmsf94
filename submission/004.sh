b0ca3c17b0b03508f916378a6e6bc54cde4b6650
Mateus Figueiredo <mateusmelosf@gmail.com>
2024-12-01 17:43:52 -0300

    4 questao

--- submission/004.sh
+++ submission/004.sh
@@ -1,19 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Extended public key
-XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+# The extended public key
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 
-# Index
-INDEX=100
+# Use a placeholder fingerprint (00000000) and specify the derivation path
+descriptor="tr([00000000/0]$xpub/100)"
 
-# Create the descriptor for the specified index
-DESCRIPTOR="tr(${XPUB}/0/${INDEX})"
+# First get the descriptor with checksum
+descriptor_with_checksum=$(bitcoin-cli getdescriptorinfo "$descriptor" | jq -r '.descriptor')
 
-# Validate the descriptor and get the checksum
-DESCRIPTOR_WITH_CHECKSUM=$(bitcoin-cli getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
-
-# Use the descriptor to derive the address
-ADDRESS=$(bitcoin-cli deriveaddresses "$DESCRIPTOR_WITH_CHECKSUM" | jq -r '.[0]')
-
-# Print the resulting Taproot address
-echo ${ADDRESS}
+# Use bitcoin-cli's deriveaddresses to get the taproot address
+bitcoin-cli deriveaddresses "$descriptor_with_checksum"
