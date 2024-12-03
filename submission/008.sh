c6b948c6537f1a5e18ce5717e00e495c43123191
Mateus Figueiredo <mateusmelosf@gmail.com>
2024-12-03 13:47:00 -0300

    8 questao

--- submission/008.sh
+++ submission/008.sh
@@ -7,6 +7,11 @@ raw_tx=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e
 decoded_tx=$(bitcoin-cli decoderawtransaction $raw_tx)
 
 # Extract the public key from the txinwitness field of input 0
-public_key=$(echo $decoded_tx | jq -r '.vin[0].txinwitness[-1]')
+# The public key is typically the second-to-last element in the txinwitness array
+public_key_script=$(echo $decoded_tx | jq -r '.vin[0].txinwitness[-1]')
+
+# Extract the public key from the script
+# Assuming the public key starts with '02', '03', or '04' and is 33 or 65 bytes long
+public_key=$(echo $public_key_script | grep -oE '02[0-9a-f]{64}|03[0-9a-f]{64}|04[0-9a-f]{128}' | head -n 1)
 
 echo $public_key
